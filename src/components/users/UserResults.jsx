6338c9c8f0a739d41fda0b8af2ccd0243b5a2ca6
Adir Noyman <45500652+AdirNoyman@users.noreply.github.com>
2022-04-26 22:30:31 +0300

    Add files via upload

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,35 +1,20 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useContext } from 'react';
 import Spinner from '../layout/Spinner';
+import UserItem from './UserItem';
+import GithubContext from '../../context/github/GithubContext';
 
 const UserResults = () => {
-	const [users, setUsers] = useState([]);
-	const [loading, setLoading] = useState(true);
+	const { users, loading, fetchUsers } = useContext(GithubContext);
 
 	useEffect(() => {
 		fetchUsers();
 	}, []);
 
-	const fetchUsers = async () => {
-		const response = await fetch(
-			`${process.env.REACT_APP_GITHUB_API_URL}/users`,
-			{
-				headers: {
-					Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-				},
-			}
-		);
-
-		const data = await response.json();
-		setUsers(data);
-		setLoading(false);
-		console.log(users);
-	};
-
 	if (!loading) {
 		return (
 			<div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
 				{users.map(user => {
-					return <h3>{user.login}</h3>;
+					return <UserItem key={user.id} user={user} />;
 				})}
 			</div>
 		);
